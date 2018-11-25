649c4cad4e4b03e41adfe611b37fe10069951308
ChrisBenua <kpbenua@edu.hse.ru>
2018-11-25 19:34:26 +0300

    Initial Commit

---
+++ Stock App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stock App
+//
+//  Created by Ирина Улитина on 25/11/2018.
+//  Copyright © 2018 Christian Benua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
